f6df90cf9bce2cc53244e61c1a0fb22c82950b9b
CynthiaNwa <nwacynti25@gmail.com>
2026-03-24 16:04:55 +0100

    Fixes

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(helmet());
 // CORS configuration
 app.use(
   cors({
-    origin: env.CORS_ORIGIN,
+    origin: env.CORS_ORIGIN || '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
